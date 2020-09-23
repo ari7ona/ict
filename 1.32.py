d9165a4886c1ab1eedaa37f5ba931685a7196164
ari7ona <70906516+ari7ona@users.noreply.github.com>
2020-09-23 19:44:35 +0600

    Delete 1.32.py

--- 1.32.py
+++
@@ -1,4 +0,0 @@
-number1 = int(input('Enter First number : '))
-number2 = int(input('Enter Second number : '))
-number3 = int(input('Enter Third number : '))
-print("Sorted numbers: " + str(min(number1, number2, number3)), str(number1+number2+number3-min(number1, number2, number3)-max(number1, number2, number3)), str(max(number1, number2, number3)))
