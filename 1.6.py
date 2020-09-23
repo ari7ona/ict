572412496852c872477edd85fd2d05a08b6c051b
ari7ona <70906516+ari7ona@users.noreply.github.com>
2020-09-23 19:45:02 +0600

    Delete 1.6.py

--- 1.6.py
+++
@@ -1,5 +0,0 @@
-charge = float(input("Enter you charge of the food: "))
-tip = 0.18 * charge
-tax = 0.10 * charge
-total = charge + tip + tax
-print("Charge: $" + "{0:.2f}".format(float(charge)), "Tip: $" + "{0:.2f}".format(float(tip)), "Tax: $" + "{0:.2f}".format(float(tax)), "Total: $" + "{0:.2f}".format(float(total)))
