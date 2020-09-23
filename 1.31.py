f7ee5cd5bdeffea1c0027a36853ec970aa1104ab
ari7ona <70906516+ari7ona@users.noreply.github.com>
2020-09-23 19:44:07 +0600

    Delete 1.31.py

--- 1.31.py
+++
@@ -1,8 +0,0 @@
-def getSum(n):
-    sum = 0
-    while (n != 0):
-        sum = sum + int(n % 10)
-        n = int(n / 10)
-    return sum
-n = int(input("Enter 4 digit number: "))
-print(getSum(n))
