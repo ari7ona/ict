1b2b13718e6bbe73b97abbf13a86aa7ea1c3e3c7
ari7ona <70906516+ari7ona@users.noreply.github.com>
2020-09-23 19:43:57 +0600

    Delete 1.30.py

--- 1.30.py
+++
@@ -1,5 +0,0 @@
-pascal = int(input("Enter pressure in kilopascals: "))
-psq = pascal*0.145
-mer = pascal*7.5
-atm = 0.009
-print("PSQ: " + str(psq), "Mercury: " + str(mer), "Atmosphere: " + str(atm))
