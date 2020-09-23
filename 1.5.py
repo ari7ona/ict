057115358c0c48ed4bc6ffa94f8cffb1e03a59f8
ari7ona <70906516+ari7ona@users.noreply.github.com>
2020-09-23 19:44:53 +0600

    Delete 1.5.py

--- 1.5.py
+++
@@ -1,4 +0,0 @@
-s_c = int(input("Enter the number of small containers: "))
-l_c = int(input("Enter the number of large containers: "))
-refund = (s_c * 0.10) + (l_c * 0.25)
-print("Your refund is $" + "{0:.2f}".format(float(refund)))
