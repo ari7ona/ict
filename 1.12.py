10d1fa104d6830c8491b7453a1d69a909e003aae
ari7ona <70906516+ari7ona@users.noreply.github.com>
2020-09-23 19:40:23 +0600

    Delete 1.12.py

--- 1.12.py
+++
@@ -1,7 +0,0 @@
-import math
-t1 = float(input("Enter the latitude of the first point: "))
-g1 = float(input("Enter the longitude of the first point: "))
-t2 = float(input("Enter the latitude of the second point: "))
-g2 = float(input("Enter the longitude of the second point: "))
-distance = 6371.01 * math.acos(math.sin(t1)*math.sin(t2) + math.cos(t1)*math.cos(t2)*math.cos(g1-g2))
-print("The distance between two point is " + str(distance) + " kilometers")
